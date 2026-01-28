a07497a369fbfaea3044d7d95879a249a0760e87
scout <202350496@jbnu.ac.kr>
2026-01-28 17:49:23 +0900

    updated tag code for multi tag function

--- scripts/esp32/tag_code/tag_code.ino
+++ scripts/esp32/tag_code/tag_code.ino
@@ -2,13 +2,21 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
-HardwareSerial MySerial(2);  // Use UART2 (pins 16=RX, 17=TX by default)
+#define DISTANCES_PER_ANCHOR_PER_CYCLE 3
+#define NUMBER_OF_ANCHORS 4
+
+HardwareSerial MySerial(2);
 const char* ssid = "iptime11";
 const char* password = "korea123#";
 
 const char* jetson_ip = "192.168.0.100";
 const int jetson_port = 5005;
 String line = "";
+
+const int esp_listen_port = 5006;
+
+unsigned char count_sent_fixes = 0;
+String TAG_ADDRESS = "10";
 
 WiFiUDP udp;
 
@@ -26,11 +34,22 @@ void send(String msg) {
   MySerial.write(msg.c_str());
 }
 
-void readTag() {
-  if (MySerial.available() > 0) {
-    String data = MySerial.readString();
-    Serial.println(data);
-  }
+String parseCmd(const String cmd) {
+  int dash_idx = cmd.indexOf('-');
+  int colon_idx = cmd.indexOf(':');
+  if (dash_idx < 0 || colon_idx < 0 || colon_idx <= dash_idx) return "error";
+
+  int end_idx = cmd.indexOf('\r', colon_idx + 1);
+  if (end_idx < 0) end_idx = cmd.indexOf('\n', colon_idx + 1);
+  if (end_idx < 0) end_idx = cmd.length();
+
+  String addr = cmd.substring(dash_idx + 1, colon_idx);      // "10"/"20"
+  String verb = cmd.substring(colon_idx + 1, end_idx);       // "START"/"STOP"
+  verb.trim();                                               // remove whitespace
+
+  if (verb == "START") return "ST:" + addr;
+  if (verb == "STOP")  return "SP:" + addr;
+  return "error";
 }
 
 void setup() {
@@ -39,37 +58,109 @@ void setup() {
 
   WiFi.begin(ssid, password);
 
+  unsigned long start_time = millis();
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+    if (millis() - start_time > 10000) {
+      Serial.println("\nWiFi connection failed!");
+      return;
+    }
   }
   Serial.println("\nWiFi Connected!");
 
   Serial.println("ESP32 started, listening on Serial2 (pins 16=RX, 17=TX)");
   delay(100);
-  talk("AT+anchor_tag=0,20\r\n");
+  talk("AT+anchor_tag=0," + TAG_ADDRESS + "\r\n");
   delay(5000);
   talk("AT+RST\r\n");
   delay(5000);
-  talk("AT+interval=50\r\n");
+  talk("AT+interval=5\r\n");
   delay(5000);
-  talk("AT+switchdis=1\r\n");
+  if (TAG_ADDRESS.equals("10")) {
+    talk("AT+switchdis=1\r\n");
+  } else if (TAG_ADDRESS.equals("20")) {
+    talk("AT+switchdis=0\r\n");
+  }
   delay(5000);
+  udp.begin(esp_listen_port);
+
+  delay(10);
+
+  udp.beginPacket(jetson_ip, jetson_port);
+  line = "ADDRESS-" + TAG_ADDRESS + ":HELLO\r\n";
+  udp.write((const uint8_t*)line.c_str(), line.length());
+  udp.endPacket();
+  Serial.println("Sent: " + line);
+  line = "";
 }
 
 void loop() {
   while (MySerial.available()) {
     char c = MySerial.read();
     if (c == '\n') {
-      // full message received
       udp.beginPacket(jetson_ip, jetson_port);
       udp.write((const uint8_t*)line.c_str(), line.length());
       udp.endPacket();
       Serial.println("Sent: " + line);
-      line = "";                    // clear for next line
+      line = "";
+      count_sent_fixes++;
     } else {
       line += c;
-      if (line.length() > 512) line = "";  // safety overflow guard
+      if (line.length() > 512) {
+        Serial.println("Warning: Line exceeded max length, discarding message");
+        line = "";
+      }
+    }
+  }
+  if (count_sent_fixes >= DISTANCES_PER_ANCHOR_PER_CYCLE * NUMBER_OF_ANCHORS) {
+    count_sent_fixes = 0;
+    talk("AT+switchdis=0\r\n");
+    delay(500);
+    talk("AT+RST\r\n");
+    delay(1000);
+    line = "ADDRESS-" + TAG_ADDRESS + ":FINISHED\r\n";
+    udp.beginPacket(jetson_ip, jetson_port);
+    udp.write((const uint8_t*)line.c_str(), line.length());
+    udp.endPacket();
+    Serial.println("Sent: " + line);
+    line = "";
+  }
+
+  unsigned long startTime = millis();
+  while (millis() - startTime < 1000) {
+    if (udp.parsePacket() > 0) {
+      while (udp.available()) {
+        char udp_char = udp.read();
+        line += udp_char;
+        
+        if (udp_char == '\n') {
+          String jetson_cmd = parseCmd(line);
+          if (jetson_cmd.startsWith("ST") && TAG_ADDRESS.equals(jetson_cmd.substring(jetson_cmd.length() - 2))) {
+            talk("AT+anchor_tag=0," + TAG_ADDRESS + "\r\n");
+            delay(500);
+            talk("AT+RST\r\n");
+            delay(1000);
+            talk("AT+switchdis=1\r\n");
+            delay(500);
+            break;
+          } else if (jetson_cmd.startsWith("SP") && TAG_ADDRESS.equals(jetson_cmd.substring(jetson_cmd.length() - 2))) {
+            talk("AT+switchdis=0\r\n");
+            delay(500);
+            talk("AT+RST\r\n");
+            delay(1000);
+            break;
+          } else {
+            Serial.println("ERROR: Wrong command or address\r");
+          }
+          line = "";
+        }
+
+        if (line.length() > 512) {
+          Serial.println("WARNING: Line exceeded max length, discarding message\r");
+          line = "";
+        }
+      }
     }
   }
 }
